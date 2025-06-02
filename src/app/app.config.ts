8d1425eb465742988cde72866d03d20e6e033ca5
Andy1825 <andresjp0415@gmail.com>
2025-06-02 15:20:56 -0500

    Correcion error login

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,15 @@ import {provideRouter, withComponentInputBinding} from '@angular/router';
 import { routes } from './app.routes';
 import {HttpClientModule} from '@angular/common/http';
 
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './interceptors/auth.interceptor';
+
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withComponentInputBinding()),
+    provideHttpClient(withInterceptors([authInterceptor])),
     importProvidersFrom(HttpClientModule)
   ]
 };
