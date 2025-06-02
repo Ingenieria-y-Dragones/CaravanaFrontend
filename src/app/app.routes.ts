0a48ce42e307d5bf0e4483d1807568d8c0cc2a46
Miguel Ángel Márquez Posso <marquezmiguel9012@gmail.com>
2025-06-02 14:16:15 -0500

    Frontend implementation.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,13 +5,16 @@ import {RutaListaComponent} from './ruta/ruta-lista/ruta-lista.component';
 import {RutaVistaComponent} from './ruta/ruta-vista/ruta-vista.component';
 import {RutaEditarComponent} from './ruta/ruta-editar/ruta-editar.component';
 import {CaravanaVistaComponent} from './caravana/caravana-vista/caravana-vista.component';
+import {LoginComponent} from './security/login/login.component';
+import {authGuard} from './guards/auth.guard';
 
 export const routes: Routes = [
-  {path: '', component: MenuVistaComponent},
-  {path: 'ciudad/vista/:id', component: CiudadVistaComponent},
-  {path: 'ruta/lista/:id', component: RutaListaComponent}, // Equivalente en Spring Boot: /ruta/lista
-  {path: 'ruta/vista/:id', component: RutaVistaComponent}, // Equivalente en Spring Boot: /ruta/vista/{id}
-  {path: 'ruta/editar/:id', component: RutaEditarComponent }, // Equivalente en Spring Boot: /ruta/editar/{id}
-  {path: 'caravana/vista/:id', component: CaravanaVistaComponent}, // Equivalente en Spring Boot: /caravana/vista/{id}
-  //{path: '', pathMatch: 'full', redirectTo: 'ruta/lista'} // Equivalente en Spring Boot: /ruta/lista
+  {path: 'login', component: LoginComponent },
+  //{path: '', component: MenuVistaComponent},
+  {path: 'ciudad/vista/:id', component: CiudadVistaComponent, canActivate: [authGuard]},
+  {path: 'ruta/lista/:id', component: RutaListaComponent, canActivate: [authGuard]}, // Equivalente en Spring Boot: /ruta/lista
+  {path: 'ruta/vista/:id', component: RutaVistaComponent, canActivate: [authGuard]}, // Equivalente en Spring Boot: /ruta/vista/{id}
+  {path: 'ruta/editar/:id', component: RutaEditarComponent, canActivate: [authGuard] }, // Equivalente en Spring Boot: /ruta/editar/{id}
+  {path: 'caravana/vista/:id', component: CaravanaVistaComponent, canActivate: [authGuard]}, // Equivalente en Spring Boot: /caravana/vista/{id}
+  {path: '', pathMatch: 'full', redirectTo: 'login'} // Equivalente en Spring Boot: /ruta/lista
 ];
